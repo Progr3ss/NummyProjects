3bcb2f9f98715a2ffc6029f2d7f0f795bb65a758
Martin Chibwe <progr3ss@Martins-MacBook-Pro.local>
2018-12-02 02:00:22 -0800

    tableview bug

--- Nummy/Controller/ForYouViewController.swift
+++ Nummy/Controller/ForYouViewController.swift
@@ -9,22 +9,75 @@
 import UIKit
 
 class ForYouViewController: UIViewController {
+    
 
+    @IBOutlet weak var forYouTableView: UITableView!
+    
+    
+   
+    
+    //2
+    private let items = ["item 1", "item 2", "item 3", "item 4", "item 5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+//        forYouTableView.reloadData()
+//       self.forYouTableView.register(ForYouTableViewCell.self, forCellReuseIdentifier: "forYouCell")
         // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidAppear(_ animated: Bool) {
+//        forYouTableView.reloadData()
+//        self.forYouTableView.register(ForYouTableViewCell.self, forCellReuseIdentifier: "forYouCell")
+        
     }
-    */
+
+
+  
 
 }
+
+extension ForYouViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = Bundle.main.loadNibNamed("ForYouTableViewCell", owner: self, options: nil)?.first as! ForYouTableViewCell
+        
+//
+        cell.recipeNameLabel.text = "test1"
+//        cell.sourceNameLabel.text = "test2"
+//        self.forYouTableView.register(ForYouTableViewCell.self, forCellReuseIdentifier: "forYouCell")
+//        self.forYouTableView.register(ForYouTableViewCell.self, forCellReuseIdentifier: "forYouCell")
+//        let cell = forYouTableView.dequeueReusableCell(withIdentifier: "forYouCell") as! ForYouTableViewCell
+        
+//        cell.textLabel?.text = items[indexPath.row]
+        
+//        self.forYouTableView.register(ForYouTableViewCell.self, forCellReuseIdentifier: "forYouCell")
+//        let cell = forYouTableView.dequeueReusableCell(withIdentifier: "forYouCell", for: indexPath) as! ForYouTableViewCell
+        
+//        cell.recipeLayout.recipeNameLabel.text = "test1"
+//        let cell1 = self.forYouTableView.regis
+//        cell.recipeNameLabel.text = "test1"
+//        cell.textLabel?.text = "test1"
+//        cell.sourceNameLabel.text = "test2"
+//        cell.
+//        cell.recipeNameLabel.text = "test2"
+//        cell.recipeNameLabel
+//        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 530
+    }
+    
+    
+    
+}
+
+
